9f084e3df1c33a2c414f0d9cc9ee1fc2dfccda82
Rocky-Sales <rocky.t@myyahoo.com>
2025-03-04 03:18:49 +0530

    Delete src directory

--- src/components/ui/button.js
+++
@@ -1,8 +0,0 @@
-export function Button({ children, className, ...props }) {
-    return (
-      <button className={`px-4 py-2 bg-yellow-500 text-black rounded-lg ${className}`} {...props}>
-        {children}
-      </button>
-    );
-  }
-  
